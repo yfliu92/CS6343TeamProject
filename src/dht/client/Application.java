7c74e7e03c048f593a7a8fcd7da0402ede32ade0
mike-holcomb <mike-holcomb@users.noreply.github.com>
2018-10-07 16:42:16 -0500

    Cleaned up log files, added more runs

--- src/dht/client/Application.java
+++ src/dht/client/Application.java
@@ -32,7 +32,7 @@ public class Application {
 					break;	
 			}
 			
-			for(int i =0; i < 10; i++)
+			for(int i =0; i < 1000; i++)
 			{
 				client.randomWrite();
 			}
--- test/test_single.sh
+++ test/test_single.sh
@@ -8,9 +8,9 @@ echo "-- Building Client"
 ant -f ../ant/build-client-single.xml
 
 echo "Launching server"
-java -jar ../build/Single/SingleServer.jar -m single -h 127.0.0.1 -p 8100 > single_server_output.log 2>single_server_debug.log &
+java -jar ../build/Single/SingleServer.jar -m single -h 127.0.0.1 -p 8100 > log/single_server_output.log 2>log/single_server_debug.log &
 
 echo "Launching clients"
-java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C101 > single_client_output.log &
-java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C102 >> single_client_output.log $
-java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C103 >> single_client_output.log &
+java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C101 > log/single_client_output.log &
+java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C102 >> log/single_client_output.log $
+java -jar ../build/Single/SingleClient.jar -m single -h 127.0.0.1 -p 8100 -i C103 >> log/single_client_output.log &
