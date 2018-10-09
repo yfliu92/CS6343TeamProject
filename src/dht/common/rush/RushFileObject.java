63152cf457eba7689d2adac3a6a6a5e5e262dabd
LIU <liuyf211@gmail.com>
2018-10-09 15:20:29 -0500

    Add getter setter method for private attrs

--- src/dht/common/rush/RushFileObject.java
+++ src/dht/common/rush/RushFileObject.java
@@ -11,9 +11,29 @@ public class RushFileObject extends FileObject {
         super();
     }
 
+    public RushFileObject(String filename, int version, int size, long lastModified) {
+        super(filename, version, size, lastModified);
+    }
+
     public RushFileObject(String filename, int version, int size, long lastModified, int totalReplicas, int replicaID) {
         super(filename, version, size, lastModified);
         this.totalReplicas = totalReplicas;
+        this.replicaID = replicaID;
+    }
+
+    public int getTotalReplicas() {
+        return totalReplicas;
+    }
+
+    public void setTotalReplicas(int totalReplicas) {
+        this.totalReplicas = totalReplicas;
+    }
+
+    public int getReplicaID() {
+        return replicaID;
+    }
+
+    public void setReplicaID(int replicaID) {
         this.replicaID = replicaID;
     }
 }
