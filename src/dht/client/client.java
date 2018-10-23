bdd74ed021b214669d1140654beeacb0d14c9869
sidli <messi.number@gmail.com>
2018-10-22 23:36:12 -0500

    client

--- src/dht/client/client.java
+++ src/dht/client/client.java
@@ -1,4 +1,4 @@
-package dht.client;
+//package dht.client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,47 +7,119 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.Date;
+import java.io.Console;
+import java.util.Vector;
+import java.lang.String;
+import java.io.File;
+import java.io.FileReader;
 
 import javax.swing.JOptionPane;
+import java.util.HashMap;
 
 public class client {
-
-	public static String getCommandStr(String[] args) {
-		if (args == null || args.length == 0) return "";
-		StringBuilder command = new StringBuilder();
-		for(int i = 0; i < args.length; i++) {
-			if (command.length() > 0) {
-				command.append(" ");
-			}
-			command.append(args[i]);
-		}
-		return command.toString();
-	}
-	
+    String getFileServerFromCentral(Socket centralServer, String filename)
+    {
+        try(PrintWriter out = new PrintWriter(centralServer.getOutputStream(), true))
+        {
+            out.println("query " + filename);
+            BufferedReader input = new BufferedReader(new InputStreamReader(centralServer.getInputStream()));
+            String answer = input.readLine();
+            String timeReceived = new Date().toString();
+            System.out.println(timeReceived + " -- response Received: " + answer);
+            return answer;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    String getFileServerFromDistribute()
+    {
+        return "";
+    }
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		if (args.length == 0) {
-			System.out.println("You need to enter at least one parameter!");
-			System.out.println("Enter command 'find XXX' or 'loadbalance XXX'");
-			return;
-		}
+        //cache map from filename to server(ip+port), much to less
+        HashMap<String, String> cache1 = new HashMap<String, String>();
+        //cache map from server to socket, one to one
+        HashMap<String, Socket> cache2 = new HashMap<String, Socket>();
 
-		String serverAddress = "localhost";
-        Socket s = new Socket(serverAddress, 9090);
-        PrintWriter out =
-                new PrintWriter(s.getOutputStream(), true);
-            out.println(getCommandStr(args) + " | X");
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = input.readLine();
-//        JOptionPane.showMessageDialog(null, answer);
-        String timeReceived = new Date().toString();
-        System.out.println(timeReceived + " -- response Received: " + answer);
-        System.exit(0);
-		
+        //Socket centralServer = new Socket("localhost", 9090);
+        Console console = System.console();
+        Vector<String> cmds = new Vector<String>();
+        while(true)
+        {   
+            if(cmds.isEmpty() == true)
+            {   
+                String cmd = console.readLine("Input your command (exit/write file/read file/get cmd_file):");
+                cmds.addElement(cmd);
+            }   
+            String cmd = cmds.remove(0);
+            System.out.println(cmd);
+            if(cmd.equals("exit"))
+            {   
+                System.exit(0);
+            }   
+            else if(cmd.startsWith("get"))
+            {   
+                String filename = cmd.split(" ")[1];
+                System.out.println(filename);
+                File file = new File(filename);
+                BufferedReader reader = null;
+                try
+                {
+                    reader = new BufferedReader(new FileReader(file));
+                    String tempString;
+                    while ((tempString = reader.readLine()) != null)
+                    {
+                        System.out.println(tempString);
+                        cmds.addElement(tempString);
+                    }
+                }
+                catch(IOException e)
+                {
+                }
+                finally
+                {
+                    if(reader != null)
+                    {
+                        try
+                        {
+                            reader.close();
+                        }
+                        catch (IOException e1)
+                        {
+                        }
+                    }
+                }
+            }
+            else if(cmd.startsWith("read") || cmd.startsWith("write"))
+            {
+                String filename = cmd.split(" ")[1];
+                if(!cache1.containsKey(filename))
+                {
+                    //String serverAddress = getFileServerFromCentral(centralServer, filename);
+                    //String serverAddress = getFileServerFromDistribute();
+		            String serverAddress = "localhost 9090";
+                    cache1.put(filename, serverAddress);
+                    if(!cache2.containsKey(serverAddress))
+                    {
+                        String info[] = serverAddress.split(" ");
+                        Socket s_tmp = new Socket(info[0], Integer.parseInt(info[1]));
+                        cache2.put(serverAddress, s_tmp);
+                    }
+                }
+                Socket s = cache2.get(cache1.get(filename));
+                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                out.println(cmd);
+                BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String answer = input.readLine();
+                String timeReceived = new Date().toString();
+                System.out.println(timeReceived + " -- response Received: " + answer);
+            }
+            else
+            {
+                System.out.println("Unknow Commands");
+            }
+        }
 	}
-	
-
-
 }
