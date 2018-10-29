4f7366d0663165fdab0d304867ec44891a006560
cynthiayou <yxqcynthia@gmail.com>
2018-10-29 09:25:27 -0500

    added dataTransfer() method to return detailed description of data transfered, fixed a bug in loadBalance() method

--- src/dht/elastic_DHT_centralized/Proxy.java
+++ src/dht/elastic_DHT_centralized/Proxy.java
@@ -45,22 +45,30 @@ public class Proxy{
         this.lookupTable = lookupTable;
     }
 
+    public String dataTransfer(String fromID, String toID, int hash){
+        return "From " + fromID + " to " + toID + ": transferring data for bucket " + Integer.toString(hash);
+    }
+
     // Add a physical node without specifying for what range of buckets
     public String addNode(String ip, int port){
         // ID of the new node will be automatically created by the PhysicalNode constructor
         PhysicalNode newNode = new PhysicalNode(ip, port, "active");
         String id = newNode.getId();
         lookupTable.getPhysicalNodesMap().put(id, newNode);
-        int loadPerNode = (lookupTable.getBucketsTable().size() / lookupTable.getPhysicalNodesMap().size()) * Config.NUM_OF_REPLICAS;
+        int loadPerNode = (lookupTable.getBucketsTable().size() / lookupTable.getPhysicalNodesMap().size());
         // Use this new node as a replica for the first loadPerNode buckets in the bucketsTable
+        String result = "";
         for (int i = 0; i < loadPerNode; i++){
-            lookupTable.getBucketsTable().get(i).put(id, id);
+            HashMap<String, String> replicas = lookupTable.getBucketsTable().get(i);
+            String fromID = replicas.entrySet().iterator().next().getKey();
+            replicas.put(id, id);
+            result += dataTransfer(fromID, id, i);
         }
 
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         lookupTable.setEpoch(timestamp.getTime());
 
-        return "true|node " + id + "is added successfully.";
+        return "true|node " + id + "is added successfully.\n" + result;
     }
 
     // Add a physical node, clearly specify for what range of buckets it will serve as a replica
@@ -69,14 +77,18 @@ public class Proxy{
         String id = newNode.getId();
         lookupTable.getPhysicalNodesMap().put(id, newNode);
         // Use this new node as a replica for the first loadPerNode buckets in the bucketsTable
+        String result = "";
         for (int i = start; i < end; i++){
-            lookupTable.getBucketsTable().get(i).put(id, id);
+            HashMap<String, String> replicas = lookupTable.getBucketsTable().get(i);
+            String fromID = replicas.entrySet().iterator().next().getKey();
+            replicas.put(id, id);
+            result += dataTransfer(fromID, id, i);
         }
 
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         lookupTable.setEpoch(timestamp.getTime());
 
-        return "true|node " + id + "is added successfully.";
+        return "true|node " + id + "is added successfully.\n" + result;
     }
     public String deleteNode(String ip, int port) {
         String nodeID = ip + Integer.toString(port);
@@ -90,35 +102,41 @@ public class Proxy{
         node.setStatus("inactive");
         // Get the bucketsTable
         ArrayList<HashMap<String, String>> table = lookupTable.getBucketsTable();
+        String result = "";
         for (int i = 0; i < table.size(); i++) {
             if (table.get(i).get(nodeID) == null)
                 continue;
             else {
+                // Remove this node ID from the bucket's replicas
                 table.get(i).remove(nodeID);
+                // Since the node is deleted/failed, must find another existing replica to transfer data to the newly added replica
+                String fromID = table.get(i).entrySet().iterator().next().getKey();
                 // Get a list of all physical node ids
                 Set<String> idSet = lookupTable.getPhysicalNodesMap().keySet();
                 List<String> idList = new ArrayList<>(idSet);
+
                 // Randomly select a node to replace the deleted node
-                String result;
                 do {
                     Random ran = new Random();
                     String id = idList.get(ran.nextInt(idList.size()));
                     result = table.get(i).put(id, id);
                 } while (result != null);
+
+                result += dataTransfer(fromID, id, i);
             }
         }
-        return "true|the node of " + nodeID + "has been successfully removed";
+        return "true|the node of " + nodeID + "has been successfully removed\n" + result;
     }
     public String loadBalance(String fromIP, int fromPort, String toIP, int toPort, int numOfBuckets){
-        String fromID = fromIP + Integer.toString(fromPort);
-        String toID = toIP + Integer.toString(toPort);
+        String fromID = fromIP + "-" + Integer.toString(fromPort);
+        String toID = toIP + "-" + Integer.toString(toPort);
         // Check if the Physical node of fromID exists or not
         PhysicalNode fromNode = lookupTable.getPhysicalNodesMap().get(fromID);
         if (fromNode == null){
             return "false|This node of " + fromID + "doesn't exist!";
         }
         PhysicalNode toNode = lookupTable.getPhysicalNodesMap().get(fromID);
-        if (fromNode == null){
+        if (toNode == null){
             // Create a new physical node of toID
             PhysicalNode newNode = new PhysicalNode(toIP, toPort, "active");
             lookupTable.getPhysicalNodesMap().put(toID, newNode);
@@ -127,17 +145,19 @@ public class Proxy{
         ArrayList<HashMap<String, String>> table = lookupTable.getBucketsTable();
         int count = numOfBuckets;
         int i = 0;
+        String result = "";
         while (count > 0){
             if (table.get(i).get(fromID) == null)
                 i++;
             else {
                 table.get(i).remove(fromID);
                 table.get(i).put(toID, toID);
+                result += dataTransfer(fromID, toID, i) + " ";
                 i++;
                 count--;
                 }
         }
-        return "true|loadBalance from " + fromID + " to " + toID + " for " + numOfBuckets + " buckets has been completed.";
+        return "true|loadBalance from " + fromID + " to " + toID + " for " + numOfBuckets + " buckets: " + result;
     }
 
 }
