3b4b0580d5f94a8ec52c193b9053fec1978b649e
LIU <liuyf211@gmail.com>
2018-11-09 15:04:34 -0600

    Change response to response2

--- src/dht/server/method/ReadBaseMethod.java
+++ src/dht/server/method/ReadBaseMethod.java
@@ -11,15 +11,16 @@ import dht.common.repository.Repository;
 import dht.common.request.ReadRequest;
 import dht.common.request.Request;
 import dht.common.response.Response;
+import dht.common.response.Response2;
 
 public class ReadBaseMethod extends Method {
 	@Override
-	public Response run(Request req)
+	public Response2 run(Request req)
 	{
 		Configuration config = Configuration.getInstance();
 		Context context = Context.getInstance();
 		Repository repo = Repository.getInstance();
-		Response res = new Response(config.getNodeId(), req.from, req.id, "read");
+		Response2 res = new Response2(config.getNodeId(), req.from, req.id, "read");
 		ReadRequest readReq = (ReadRequest) req;
 		
 		try {
--- src/dht/server/method/WriteBaseMethod.java
+++ src/dht/server/method/WriteBaseMethod.java
@@ -10,15 +10,16 @@ import dht.common.repository.Repository;
 import dht.common.request.Request;
 import dht.common.request.WriteRequest;
 import dht.common.response.Response;
+import dht.common.response.Response2;
 
 public class WriteBaseMethod extends Method {
 	@Override
-	public Response run(Request req)
+	public Response2 run(Request req)
 	{
 		Configuration config = Configuration.getInstance();
 		Context context = Context.getInstance();
 		Repository repo = Repository.getInstance();
-		Response res = new Response(config.getNodeId(), req.from, req.id, "write");
+		Response2 res = new Response2(config.getNodeId(), req.from, req.id, "write");
 		
 		
 		FileObject fo = ((WriteRequest) req).getFileObject();
